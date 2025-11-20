c5ca6e1fd403a79f518e946544f039ba7f514381
Jimmy549 <ehmitservices1@gmail.com>
2025-11-20 17:42:31 +0500

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+        '3xl': '1600px',
+      },
+      fontSize: {
+        'xs-mobile': ['0.75rem', { lineHeight: '1rem' }],
+        'sm-mobile': ['0.875rem', { lineHeight: '1.25rem' }],
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+      },
+    },
   },
   plugins: [],
 }
